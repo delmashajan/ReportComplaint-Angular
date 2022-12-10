d81a8bc13d41d2a05476bd8a02aa8fe7adfb50f2
delmashajan <delmashajan@gmail.com>
2022-12-10 22:31:46 +0530

    user login

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,5 +8,9 @@ export class ApiService {
 
   constructor(private http:HttpClient) { }
 
-  
+  userLogin=(data:any)=>{
+    return this.http.post("http://localhost:8080/userlogin",data);
+  }
+
+
 }
